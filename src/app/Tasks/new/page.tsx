92a031885f0172802e609aca6b9276d261d8d82b
V4runa <varunanda.creative@gmail.com>
2025-09-10 23:54:57 -0400

    fixed client rendering issues with tasks

--- src/app/Tasks/new/page.tsx
+++ src/app/Tasks/new/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import GlassPanel from "../../components/GlassPanel";
 import MotionDiv from "../../components/MotionDiv";
--- src/app/Tasks/page.tsx
+++ src/app/Tasks/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { apiFetch } from "../hooks/useApi";
